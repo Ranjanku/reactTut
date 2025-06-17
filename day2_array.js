2e6e51e448ce29e15d095362736f25644b70be56
Ranjan Kumar <ranjankumar9a@gmail.com>
2025-06-17 14:27:48 +0530

    some changes

--- day2_array.js
+++ day2_array.js
@@ -1,16 +1,28 @@
-const arr = [{name:'Ranjan', city : 'phagwara'}, {name:'Ravi', city: 'Delhi'}];
+// const arr = [{name:'Ranjan', city : 'phagwara'}, {name:'Ravi', city: 'Delhi'}];
 
-// when we are creating an object, it is stored in a different memory location
-// so when we compare two objects, they are not equal even if they have the same properties
-if({name:'Ranjan'} == {name:'Ranjan'}) {
-    console.log("true");
-} else {
-    console.log("false"); // false, because these are different object references
+// // when we are creating an object, it is stored in a different memory location
+// // so when we compare two objects, they are not equal even if they have the same properties
+// if({name:'Ranjan'} == {name:'Ranjan'}) {
+//     console.log("true");
+// } else {
+//     console.log("false"); // false, because these are different object references
+// }
+
+// // big mistake
+// if({} == {}) {
+//     console.log("true");
+// } else {
+//     console.log("false"); // false, because these are different object references
+
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+const arr = ["ab", "cd", "ef", "gh", "ij"];
+
+if(arr.indexOf("ab")){
+    console.log("present");
+}else {
+    console.log("not present");
 }
 
-// big mistake
-if({} == {}) {
-    console.log("true");
-} else {
-    console.log("false"); // false, because these are different object references
-}
